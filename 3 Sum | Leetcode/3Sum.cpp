05240b09c22eb6b5fd91dd16fe4991546e5940f6
Amit Pathak <amit.pathak321@gmail.com>
2022-05-14 10:57:13 +0530

    Update 3Sum.cpp

--- 3 Sum | Leetcode/3Sum.cpp
+++ 3 Sum | Leetcode/3Sum.cpp
@@ -1,24 +1,43 @@
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
-Notice that the solution set must not contain duplicate triplets.
-
- 
-
-Example 1:
-
-Input: nums = [-1,0,1,2,-1,-4]
-Output: [[-1,-1,2],[-1,0,1]]
-Example 2:
-
-Input: nums = []
-Output: []
-Example 3:
-
-Input: nums = [0]
-Output: []
- 
-
-Constraints:
-
-0 <= nums.length <= 3000
--105 <= nums[i] <= 105
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+             vector<vector<int>>result;
+        if(nums.size() <3)
+            return {};
+    
+        sort(nums.begin(), nums.end());
+        for(int i=0;i <nums.size() -2; i++){
+            if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                 int a = i;
+            int low = i + 1;
+            int high = nums.size() - 1;
+            
+            while( low < high){
+                vector<int>temp;
+               
+                if((nums[low] + nums[high]) == -nums[a]){
+                    temp.push_back(nums[a]);
+                    temp.push_back(nums[low]);
+                    temp.push_back(nums[high]);
+                    result.push_back(temp);
+                     while(low<high && nums[low] == nums[low+1]){
+                    low++;
+                }
+                  while(low<high && nums[high] == nums[high-1]){
+                    high--;
+                }
+                    low++;
+                    high--;
+                } else if((nums[low] + nums[high]) < -nums[a]){
+                    low++;
+                } else if((nums[low] + nums[high]) > -nums[a]){
+                    high--;
+                }
+            }
+            }
+           
+        
+        }
+        return result;
+    }
+};
